4c23e03767e71105fd09320dc1527b5f5ad52cb1
elfarizidan <elfarizidan@gmail.com>
2026-08-31 10:30:34 +0700

    feat: modularize Firebase configuration by moving it to a dedicated src/firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTm7ua_gy1cdvMziV65ZHzC6AXDiZ8hGY",
+  authDomain: "fix-and-call.firebaseapp.com",
+  projectId: "fix-and-call",
+  storageBucket: "fix-and-call.firebasestorage.app",
+  messagingSenderId: "276029133340",
+  appId: "1:276029133340:web:5720ca8f4ee2f1787645d9"
+};
+
+export default firebaseConfig;
